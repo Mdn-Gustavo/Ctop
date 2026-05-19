65bf89ed4b793e106600c51d5f24ba0ff437b72c
Mdn_Gustavo <holyboy02@proton.me>
2026-05-18 23:29:12 -0300

    Initialization commit

---
+++ monitor.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void get_mem_usage() {
+  FILE *fp = fopen("/proc/meminfo", "r");
+  if (fp == NULL) {
+    perror("Error opening /proc/meminfo");
+    return;
+  }
+  char line[256];
+  unsigned long mem_total = 0, mem_available = 0;
+
+  while (fgets(line, sizeof(line), fp)) {
+
+    if (strncmp(line, "MemTotal:", 9) == 0) {
+      sscanf(line,
+             "MemTotal:"
+             "%lu kB",
+             &mem_total);
+    }
+
+    // mesma coisa com o mem_free bicho
+    if (strncmp(line, "MemAvailable:", 13) == 0) {
+      sscanf(line,
+             "MemAvailable:"
+             "%lu kB",
+             &mem_available);
+    }
+    fclose(fp);
+  }
+
+  float percent =
+      ((float)mem_total - (float)mem_available) / (float)mem_total * 100;
+  const int blocks = 20;
+  int filled = (int)(mem_total - mem_available) / 0.8;
+  if (filled < 0)
+    filled = 0;
+  if (filled > blocks)
+    filled = blocks;
+
+  for (int i = 0; i <= blocks; i++) {
+  }
+  usleep(100000);
+  printf(" ");
+
+  // Exiba o resultado de um jeito "ricer": [||||      ] 40%
+}
+
+int main() {
+  printf("\033[H\033[2J");
+  printf("---HolyMonitor---\n");
+  get_mem_usage();
+  fflush(stdout);
+  return 0;
+}
